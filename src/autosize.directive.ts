0642370ee68ee3da2d4f6d24a35eec3636e80927
yedan <yedan@kyee.com.cn>
2017-11-21 10:44:24 +0800

    commiter:diana

---
+++ src/autosize.directive.ts
@@ -0,0 +1,24 @@
+import { ElementRef, HostListener, Directive} from '@angular/core';
+
+@Directive({
+    selector: 'textarea[autosize]'
+})
+
+export class Autosize {
+ @HostListener('input',['$event.target'])
+  onInput(textArea: HTMLTextAreaElement): void {
+    this.adjust();
+  }
+  constructor(public element: ElementRef){
+  }
+  ngAfterContentChecked(): void{
+    this.adjust();
+  }
+  adjust(): void{
+    let scrollPosition = window.scrollY;
+    this.element.nativeElement.style.overflow = 'hidden';
+    this.element.nativeElement.style.height = 'auto';
+    this.element.nativeElement.style.height = this.element.nativeElement.scrollHeight + "px";
+    window.scrollTo(0, scrollPosition);
+  }
+}
